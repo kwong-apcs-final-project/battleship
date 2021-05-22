e33c976ac478cc9d2c229cadef44b30bd102232a
saggins <win32prog@gmail.com>
2021-05-22 12:46:37 -0700

    Revert "Revert "Merge branch 'main' of https://github.com/kwong-apcs-final-project/battleship into main""
    This reverts commit f2078cc4c1c5b908fa441dec7a086c44434a2d09.

--- src/com/github/battleship/Boat.java
+++ src/com/github/battleship/Boat.java
@@ -1,15 +1,79 @@
 package com.github.battleship;
 
+import java.util.ArrayList;
+
+/**
+ * Class that represents a boat in the game
+ * has locations of where it is on board
+ * @author kbagal
+ *
+ */
 public class Boat {
 	
-	private Location[] locations;
-	private int numHittable;
+	private ArrayList<Location> locations;
+	private ArrayList<Location> hits;
 	
 	//ask if inputs are going to be Locations or x/y coordinates
-	public Boat(int x, int y) 
+	/**
+	 * Constructs a new Boat Object
+	 * @param boatLoc locations objects of the given Boat
+	 */
+	public Boat(ArrayList<Location> boatLoc) 
 	{
-		locations = new Location[1];
-		locations[0] = new Location(x, y);
+		locations = boatLoc;
+		hits = new ArrayList<Location>(5);
+		
+	}
+	
+	/**
+	 * Method that returns whether is given location is
+	 * on the boat or not
+	 * @param attemptToHit location to test
+	 * @return boolean if true or not
+	 */
+	public boolean isHit(Location attemptToHit) 
+	{
+		for (Location loc : locations) 
+		{
+			if (loc.same(attemptToHit)) 
+			{
+				return true;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Method to remove a given hit point from 
+	 * Available hit locations
+	 * @param attemptToHit the hit spot
+	 */
+	public void removeLoc(Location attemptToHit) 
+	{
+		for (int i = 0; i < locations.size(); i++) 
+		{
+			if (locations.get(i).same(attemptToHit)) 
+			{
+				locations.remove(i);
+				break;
+			}
+		}
+	}
+	
+	/**
+	 * Assessor method for number of hittable spots left
+	 * @return locations.size() = num of hittble spots
+	 */
+	public int spotsLeft() 
+	{
+		return locations.size();
+	}
+	
+	public ArrayList<Location> calculateHittable(Boat b)
+	{
+		//return an arrayList of even single hittable
+		//object for a given boat
+		return new ArrayList<Location>();
 	}
 	
 }
--- src/com/github/battleship/PointerBoard.java
+++ src/com/github/battleship/PointerBoard.java
@@ -1,5 +1,13 @@
 package com.github.battleship;
 
+import java.util.ArrayList;
+
 public class PointerBoard {
 
+	private ArrayList<Pointer> pboard;
+	
+	public PointerBoard() 
+	{
+		pboard = new ArrayList<Pointer>(64);
+	}
 }

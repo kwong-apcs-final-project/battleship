9fee78af1da0d5e11244bb403ee421427049a617
saggins <win32prog@gmail.com>
2021-05-22 12:42:21 -0700

    Added Functinal pionter making

--- src/resources/static/pointerBoard.js
+++ src/resources/static/pointerBoard.js
@@ -1,5 +1,20 @@
+import {newRectangle, newPointer} from  './util.js'
+
+
 var width = 1200;
 var height = 600;
+var blockSnapSize = 60;
+var shadowRectangle = new Konva.Rect({
+  x: 0,
+  y: 0,
+  width: blockSnapSize * 1,
+  height: blockSnapSize * 1,
+  fill: '#FF7B17',
+  opacity: 0.6,
+  stroke: '#CF6412',
+  strokeWidth: 3,
+  dash: [20, 2]
+});
 
 var stage = new Konva.Stage({
   container: 'pointerBoard',
@@ -7,6 +22,8 @@ var stage = new Konva.Stage({
   height: height,
 });
 var backgroundLayer = new Konva.Layer();
+stage.add(backgroundLayer);
+
 var background = new Konva.Rect( {
   width: width,
   height: height,
@@ -15,59 +32,79 @@ var background = new Konva.Rect( {
 });
 backgroundLayer.add(background);
 
+var gridLayer = new Konva.Layer();
+var padding = blockSnapSize;
+console.log(width, padding, width / padding);
+for (var i = 0; i < width / padding; i++) {
+  gridLayer.add(new Konva.Line({
+    points: [Math.round(i * padding) + 0.5, 0, Math.round(i * padding) + 0.5, height],
+    stroke: '#ddd',
+    strokeWidth: 1,
+  }));
+}
+
+//gridLayer.add(new Konva.Line({points: [0,0,10,10]}));
+for (var j = 0; j < height / padding; j++) {
+  gridLayer.add(new Konva.Line({
+    points: [0, Math.round(j * padding), width, Math.round(j * padding)],
+    stroke: '#ddd',
+    strokeWidth: 0.5,
+  }));
+}
+stage.add(gridLayer)
+var grid = new Konva.Rect({
+  width: 600,
+  height: 600,
+  x: 300,
+  fill: '#222222'
+});
+backgroundLayer.add(grid);
+
+var foreGround = new Konva.Layer();
+
+var toolBoxes = new Konva.Layer();
 var pointerRBox = new Konva.Rect({
     x: 900,
     y: 0,
     width: 300,
     height: 300,
-    fill: '#00D2FF'
+    fill: '#8E1600'
 
 });
-backgroundLayer.add(pointerRBox)
+toolBoxes.add(pointerRBox)
+pointerRBox.on('click', ()=> {
+  var pos = stage.getRelativePointerPosition();
+  newPointer(pos.x, pos.y, foreGround, stage, false, shadowRectangle);
+});
 
 var pointerBBox = new Konva.Rect({
     x: 900,
     y: 300,
     width: 300,
     height: 300,
-    fill: '#8E1600'
+    fill: '#00D2FF'
 
+});
+toolBoxes.add(pointerBBox)
+
+pointerBBox.on('click', ()=> {
+  var pos = stage.getRelativePointerPosition();
+  newPointer(pos.x, pos.y, foreGround, stage, true, shadowRectangle);
 });
 
 var boatBox = new Konva.Rect({
-  x:0,
-  y:0,
-  width:300,
-  height: 600,
-  fill: '#7E7E7E'
+    x:0,
+    y:0,
+    width:300,
+    height: 600,
+    fill: '#7E7E7E'
 })
-backgroundLayer.add(boatBox)
+toolBoxes.add(boatBox)
+stage.add(toolBoxes);
 
-backgroundLayer.add(pointerBBox)
-stage.add(backgroundLayer);
 
 var layer = new Konva.Layer();
-var rectX = stage.width() / 2 - 50;
-var rectY = stage.height() / 2 - 25;
-
-var box = new Konva.Rect({
-  x: rectX,
-  y: rectY,
-  width: 100,
-  height: 50,
-  fill: '#00D2FF',
-  stroke: 'black',
-  strokeWidth: 4,
-  draggable: true,
-});
-
-// add cursor styling
-box.on('mouseover', function () {
-  document.body.style.cursor = 'pointer';
-});
-box.on('mouseout', function () {
-  document.body.style.cursor = 'default';
-});
-
-layer.add(box);
+shadowRectangle.hide();
+layer.add(shadowRectangle);
+stage.add(foreGround);
 stage.add(layer);
---
+++ src/resources/static/util.js
@@ -0,0 +1,100 @@
+var width = 1200;
+var height = 600;
+var shadowOffset = 20;
+var tween = null;
+var blockSnapSize = 60;
+
+  
+export function newRectangle(x, y, layer, stage) {
+    let rectangle = new Konva.Rect({
+        x: x,
+        y: y,
+        width: blockSnapSize * 1,
+        height: blockSnapSize * 1,
+        fill: '#fff',
+        stroke: '#ddd',
+        strokeWidth: 1,
+        shadowColor: 'black',
+        shadowBlur: 2,
+        shadowOffset: {x : 1, y : 1},
+        shadowOpacity: 0.4,
+        draggable: true
+    });
+    rectangle.on('dragstart', (e) => {
+        shadow.show();
+        shadow.moveToTop();
+        rectangle.moveToTop();
+    });
+    rectangle.on('dragend', (e) => {
+        rectangle.position({
+        x: Math.round(rectangle.x() / blockSnapSize) * blockSnapSize,
+        y: Math.round(rectangle.y() / blockSnapSize) * blockSnapSize
+        });
+        stage.batchDraw();
+        shadow.hide();
+    });
+    rectangle.on('dragmove', (e) => {
+        shadow.position({
+        x: Math.round(rectangle.x() / blockSnapSize) * blockSnapSize,
+        y: Math.round(rectangle.y() / blockSnapSize) * blockSnapSize
+        });
+        stage.batchDraw();
+    });
+    layer.add(rectangle);
+}
+
+
+
+/**
+ * Adds Pionter to specfied Stage
+ * @param {Number} x
+ * @param {Number} y 
+ * @param {*} layer 
+ * @param {*} stage 
+ * @param {*} miss
+ * @param {*} shadow 
+ */
+export function newPointer(x, y, layer, stage, miss, shadow) {
+    let color = '#8E1600'
+    if (miss) {
+        color = '#FFFFFF'
+    }
+    let rectangle = new Konva.Rect({
+        x: x,
+        y: y,
+        width: blockSnapSize * 1/2,
+        height: blockSnapSize * 1/2,
+        fill: color,
+        stroke: '#ddd',
+        strokeWidth: 1,
+        shadowColor: 'black',
+        shadowBlur: 2,
+        shadowOffset: {x : 1, y : 1},
+        shadowOpacity: 0.4,
+        draggable: true
+    });
+    rectangle.on('dragstart', (e) => {
+        shadow.show();
+        shadow.moveToTop();
+        rectangle.moveToTop();
+    });
+    rectangle.on('dragend', (e) => {
+        let offset = blockSnapSize/4;
+        let x = (Math.round(rectangle.x() / blockSnapSize) * blockSnapSize) + offset;
+        let y = (Math.round(rectangle.y() / blockSnapSize) * blockSnapSize) + offset;
+        rectangle.position({
+        x: x,
+        y: y
+        });
+        stage.batchDraw();
+        shadow.hide();
+    });
+    rectangle.on('dragmove', (e) => {
+        shadow.position({
+        x: Math.round(rectangle.x() / blockSnapSize) * blockSnapSize,
+        y: Math.round(rectangle.y() / blockSnapSize) * blockSnapSize
+        });
+        stage.batchDraw();
+    });
+    layer.add(rectangle);
+}
